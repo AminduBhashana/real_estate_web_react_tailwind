3853b757fd59a9f7d346d3f96bf751874fab2e7a
AminduBhashana <waamidu@gmail.com>
2024-12-16 00:33:50 +0530

    complete the header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' id='Header' style={
+        {backgroundImage: "url('/header_image.jpg')"}
+    }>
+        <Navbar />
+        <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+            <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams</h2>
+            <div className='space-x-6 mt-16'>
+                <a href='#Projects' className='border border-purple-600 bg-purple-600 px-8 py-3 rounded-md'>Projects</a>
+                <a href='#Contact' className='border border-white px-8 py-3 rounded-md'>Contact Us</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
