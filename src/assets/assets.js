7da6ec8c0a6a41de7a5707ac1cb8416a606b441b
AminduBhashana <waamidu@gmail.com>
2024-12-16 01:32:28 +0530

    mobile navbar menu done

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg'
+import menu_icon from './menu-icon.svg'
+import close_icon from './close-icon.svg'
 
 export const assets = {
     logo,
+    menu_icon,
+    close_icon,
 }
