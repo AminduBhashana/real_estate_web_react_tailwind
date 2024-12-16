31f2c7d30d3ef0304f25e43cf2100d9d3b194e7b
AminduBhashana <waamidu@gmail.com>
2024-12-16 23:33:10 +0530

    header section design update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,23 +21,23 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-28 bg-transparent">
         <img src={assets.logo} alt="logo"></img>
         <ul className="hidden md:flex gap-7 text-white">
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a href="#Header" className="cursor-pointer hover:text-purple-300">
             Home
           </a>
-          <a href="#About" className="cursor-pointer hover:text-gray-400">
+          <a href="#About" className="cursor-pointer hover:text-purple-300">
             About
           </a>
-          <a href="#Projects" className="cursor-pointer hover:text-gray-400">
+          <a href="#Projects" className="cursor-pointer hover:text-purple-300">
             Projects
           </a>
           <a
             href="#Testimonials"
-            className="cursor-pointer hover:text-gray-400"
+            className="cursor-pointer hover:text-purple-300"
           >
             Testimonials
           </a>
         </ul>
-        <button className="hidden md:block bg-white px-8 py-2 rounded-full">
+        <button className="hidden md:block bg-purple-600 px-8 py-2 rounded-full text-white">
           Sign Up
         </button>
         <img
