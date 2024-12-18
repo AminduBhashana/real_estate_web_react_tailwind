7225142152607a2fe07750c4f9af48a0b81130b7
AminduBhashana <waamidu@gmail.com>
2024-12-18 07:54:11 +0530

    some footer design updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { toast } from 'react-toastify'
+import { decorate } from 'react-toastify/addons/use-notification-center'
 
 const Footer = () => {
   return (
@@ -12,10 +14,10 @@ const Footer = () => {
             <div className='w-full md:w-1/5 mb-8 md:mb-0'>
                 <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
                 <ul className='flex flex-col gap-2 text-gray-400'>
-                    <a href='#Header' className='hover:text-white'>Home</a>
-                    <a href='#About' className='hover:text-white'>About Us</a>
-                    <a href='#Contact' className='hover:text-white'>Contact Us</a>
-                    <a href='#Testimonials' className='hover:text-white'>Testimonials</a>
+                    <a href='#Header' className='hover:text-purple-200'>Home</a>
+                    <a href='#About' className='hover:text-purple-200'>About Us</a>
+                    <a href='#Contact' className='hover:text-purple-200'>Contact Us</a>
+                    <a href='#Testimonials' className='hover:text-purple-200'>Testimonials</a>
                 </ul>
             </div>
             <div className='w-full md:w-1/3'>
@@ -23,7 +25,7 @@ const Footer = () => {
                 <p className='text-gray-400 mb-4 max-w-80'>The latest news, articles and resources sent to your inbox weekly.</p>
                 <div className='flex gap-2'>
                     <input type='email' placeholder='Enter your email' className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto'/>
-                    <button className='py-2 px-4 rounded bg-purple-700 text-white'>Subscribe</button>
+                    <button onClick={() => toast.info("Have a Nice Day!" )} className='py-2 px-4 rounded bg-purple-700 text-white'>Subscribe</button>
                 </div>
             </div>
         </div>
